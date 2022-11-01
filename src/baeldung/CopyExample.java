24d0acb6417fb2f35cdc27a1773fea5909fca2af
Andrei Branza <andrei.branza@softvision.com>
2022-11-01 17:42:46 +0200

    Refactored and eliminated nested classes

--- src/baeldung/CopyExample.java
+++ src/baeldung/CopyExample.java
@@ -1,6 +1,6 @@
 package baeldung;
 
-public class CopyExample {
+ class CopyExample {
 
 	public static void main(String[] args) throws CloneNotSupportedException {
 		Person alex = new Person("Alex", "Jones", new Address("Main Street", "Main City"));
@@ -24,92 +24,4 @@ public class CopyExample {
 		
 	}
 
-}
-
-class Person implements Cloneable
-{
-	private String firstName;
-	private String lastName;
-	private Address address;
-	
-	@Override
-	public Object clone() throws CloneNotSupportedException 
-	{
-		return (Person)super.clone();
-		
-	}
-	
-	private Person() {}
-
-	public Person(String firstName, String lastName, Address address) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-	}
-	
-	
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public Address getAddress() {
-		return address;
-	}
-
-	public void setAddress(Address address) {
-		this.address = address;
-	}
-
-	@Override
-	public String toString() {
-		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + "]";
-	}
-		
-}
-
-class Address
-{
-	private String streetName;
-	private String cityName;
-	
-	public String getStreetName() {
-		return streetName;
-	}
-	public void setStreetName(String streetName) {
-		this.streetName = streetName;
-	}
-	public String getCityName() {
-		return cityName;
-	}
-	public void setCityName(String cityName) {
-		this.cityName = cityName;
-	}
-	
-	private Address() {}
-	
-	public Address(String streetName, String cityName) {
-		super();
-		this.streetName = streetName;
-		this.cityName = cityName;
-	}
-	
-	@Override
-	public String toString() {
-		return "Address [streetName=" + streetName + ", cityName=" + cityName + "]";
-	}	
-	
-	
 }
